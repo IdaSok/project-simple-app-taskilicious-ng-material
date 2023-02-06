3b92c8da3d122424659e019698b929cce1ca1148
Ida <ia.sokolow01@gmail.com>
2023-02-06 22:38:46 +0100

    task service added

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { TaskService } from 'src/app/services/task.service';
 import { CategoryModel } from '../../models/category.model';
 import { CategoryService } from '../../services/category.service';
 
@@ -22,11 +23,11 @@ export class TaskFormComponent {
 //get categories
   readonly category$: Observable<CategoryModel[]> = this._categoryService.getAll();
     
-  constructor(private _categoryService: CategoryService, private _router: Router) {
+  constructor(private _categoryService: CategoryService, private _router: Router, private _taskService: TaskService) {
   }
 //create task
     onFormSubmitted(form: FormGroup): void {
-      this._categoryService.createTask({
+      this._taskService.createTask({
         name: form.get('name')?.value,
         categoryId: form.get('categoryId')?.value,
         id: form.get('id')?.value,//come back to page
